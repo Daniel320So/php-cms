1bd64535f998cab24dc65f93865362e9e7345639
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-03-21 11:29:19 -0400

    DONE: Add Sidebar to CMS

--- admin/includes/header-left.php
+++ admin/includes/header-left.php
@@ -16,16 +16,22 @@
 <body class="body-flex">
 
   <div class="sidebar">
-    <p><?php echo $_SESSION['email']; ?></p>
-    <div class="sidebar-menu">
-      <a href="../dahsboard.php" class="sidebar-item">Dashboard</a>
-      <a href="#" class="sidebar-item">Education</a>
-      <a href="#" class="sidebar-item">Experience</a>
-      <a href="#" class="sidebar-item">Projects</a>
-      <a href="#" class="sidebar-item">Skills</a>
-      <a href="#" class="sidebar-item">Users</a>
+    <div class="sidebar-left">
+      <div class="sidebar-email">
+        <p><?php echo $_SESSION['email']; ?></p>
+      </div>
+      <div class="sidebar-menu">
+        <a href="./dahsboard.php" class="sidebar-item">Dashboard</a>
+        <a href="./educations.php" class="sidebar-item">Education</a>
+        <a href="./experiences.php" class="sidebar-item">Experience</a>
+        <a href="./projects.php" class="sidebar-item">Projects</a>
+        <a href="./skills.php" class="sidebar-item">Skills</a>
+        <a href="./users.php" class="sidebar-item">Users</a>
+      </div>
     </div>
-    <a href="logout.php">Logout</a>
+    <div class="sidebar-right">
+      <a href="logout.php">Logout</a>
+    </div>
   </div>
 
   <div class="main-wrapper">
