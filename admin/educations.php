700906f3c3a390b7ce917dc9fa92827912abda1b
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-03-24 00:37:39 -0400

    Update Educations to Card View

--- admin/educations.php
+++ admin/educations.php
@@ -3,6 +3,7 @@
 include( 'includes/database.php' );
 include( 'includes/config.php' );
 include( 'includes/functions.php' );
+include( 'includes/card.php' );
 
 secure();
 
@@ -24,62 +25,76 @@ if( isset( $_GET['delete'] ) )
 include( 'includes/header-left.php' );
 
 $query = 'SELECT *
-  FROM educations ORDER BY id';
+  FROM educations WHERE user_id = '.$_SESSION['id'].' ORDER BY id';
 $result = mysqli_query( $connect, $query );
+
+$educations = array();
+
+// gets a list of educations, and sorts them into an array based on who the educations belongs to
+
+while($education = mysqli_fetch_assoc($result))
+{
+  if(!isset($educations[$education['user_id']]))
+  {
+    $educations[$education['user_id']] = array();
+  }
+  
+  array_push($educations[$education['user_id']],$education);
+}
 
 ?>
 
 <h2>Manage Educations</h2>
 
-<table>
-  <tr>
-    <th align="center">ID</th>
-    <th align="center">User ID</th>
-    <th align="center">School Name</th>
-    <th align="center">Location</th>
-    <th align="center">Study</th>
-    <th align="center">Date</th>
-    <th align="center">Content</th>
-    <th></th>
-    <th></th>
-  </tr>
-  <?php while( $record = mysqli_fetch_assoc( $result ) ): ?>
-    <tr>
-      <td align="center"><?php echo $record['id']; ?></td>
-      <td align="center"><?php echo $record['user_id']; ?></td>
-      <td align="center"><?php echo $record['school_name']; ?></td>
-      <td align="center"><?php echo $record['location']; ?></td>
-      <td align="center"><?php echo $record['level_of_education']; ?> in <?php echo $record['field']; ?></td>
-      <td align="center"><?php echo $record['start_date']; ?> - <?php echo $record['end_date']; ?></td>
-      <td align="center">
-        <small><?php echo $record['content']; ?></small>
-      </td>
-      <td align="center"><a href="educations_edit.php?id=<?php echo $record['id']; ?>">Edit</i></a></td>
-      <td align="center">
-        <button class="delete-button" data-id="<?php echo $record['id'];?>" data-education="<?php echo $record['school_name'];?>">Delete</button>
-      </td>
-    </tr>
-  <?php endwhile; ?>
-</table>
+<?php 
 
-<p><a href="educations_add.php"><i class="fas fa-plus-square"></i> Add Education</a></p>
+  // Organizes educations based on which user they belong to
 
+  $query = 'SELECT first,last,id FROM users WHERE id = "'.$_SESSION['id'].'" ORDER BY first, last, id;';
+  $users = mysqli_query($connect, $query);
+  while($user = mysqli_fetch_assoc($users))
+  {
+
+    ?>
+    <h2><?=$user['first'].' '.$user['last']?></h2>
+    <p><a href="educations_add.php?user_id=<?=$user['id']?>"><i class="fas fa-plus-square"></i> Add Education for <?=$user['first'].' '.$user['last']?></a></p>
+    <?
+    if(!isset($educations[$user['id']])) continue;
+    ?>
+
+    <div class="card-container ">
+
+    <?php foreach( $educations[$user['id']] as $record ) {
+        content_card (
+
+          $record['id'], // item ID
+
+          "educations", // Record type
+
+          $record['school_name'], // School Name
+
+          $record['level_of_education'].' in '.$record['field'].'<br>'.$record['start_date'].'-'.$record['end_date'], // Type & url
+
+          null, // Thumbnail Link
+
+          $record['content'], // body content (limi 200 characters)
+
+          "educations_edit.php?id=".$record['id'], // "Edit" button link location
+
+          "educations.php?cmd=delete&delete=".$record['id'], // "Delete" button link location
+        );
+
+    } 
+    ?>
+    </div>
+
+    <?php
+  }
+
+?>
 
 <?php
 
 include( 'includes/footer.php' );
 
 ?>
-
-<script type="text/JavaScript">
-let deleteButtons = document.getElementsByClassName("delete-button")
-if (deleteButtons && deleteButtons.length > 0) {
-  for (let button of deleteButtons) {
-    button.onclick = () => {
-      let confirmedDelete = confirm(`Are you sure you want to delete ${button.dataset.education}?`);
-      if (!confirmedDelete) return;
-      window.location.href = `educations.php?delete=${button.dataset.id}`;
-    }
-  }
-};
-</script>
