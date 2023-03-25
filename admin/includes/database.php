018af505641c3a254ffc0da99648855813793ffc
Shavin <shavinsingh1999@gmail.com>
2023-03-24 20:15:00 -0400

    updated grid responsiveness

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,19 +1,19 @@
 <?php
 
-// $connect = mysqli_connect( 
-//     "sql209.epizy.com", // Host
-//     "epiz_33812902", // Username
-//     "TIpZqtmwZ4B1S", // Password
-//     "epiz_33812902_cms" // Database
-// );
+$connect = mysqli_connect( 
+    "sql209.epizy.com", // Host
+    "epiz_33812902", // Username
+    "TIpZqtmwZ4B1S", // Password
+    "epiz_33812902_cms" // Database
+);
 
 // (Dev) MAMP Local connection 
 
-$connect = mysqli_connect( 
-    "localhost", // Host
-    "root", // Username
-    "root", // Password
-    "epiz_33812902_cms" // Database
-);
+// $connect = mysqli_connect( 
+//     "localhost", // Host
+//     "root", // Username
+//     "root", // Password
+//     "epiz_33812902_cms" // Database
+// );
 
 mysqli_set_charset( $connect, 'UTF8' );
